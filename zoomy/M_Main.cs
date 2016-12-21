a7f745fa7e24259b784e7d4724743822ea3e3392
Keenan Smith <zoomy500@gmail.com>
2016-12-20 20:11:55 -0500

    Added options to main menu

--- zoomy/M_Main.cs
+++ zoomy/M_Main.cs
@@ -6,5 +6,37 @@ namespace zoomy
 {
     public class M_Main : MonoBehaviour
     {
+        Vector2 sp;
+        Rect menu = new Rect(10, 10, 200, 200);
+        void OnGUI()
+        {
+            if (Settings.HackEnabled && Settings.InMenu)
+            {
+                menu = GUILayout.Window(WID.MainMenu, menu, DoMenu, "Hack by IC3 + Zoomy");
+            }
+        }
+        void DoMenu(int windowID)
+        {
+            sp = GUILayout.BeginScrollView(sp);
+            if (GUILayout.Button("Radar Menu"))
+            {
+                Settings.InRadarMenu = true;
+            }
+            if (GUILayout.Button("Rear Camera Menu"))
+            {
+                Settings.InRearViewMenu = true;
+            }
+
+            GUILayout.EndScrollView();
+            GUI.DragWindow();
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F1))
+            {
+                Settings.InMenu = !Settings.InMenu;
+            }
+        }
     }
 }
