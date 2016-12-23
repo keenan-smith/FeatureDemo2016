37ce83eab8ff15d55a878aa735e8f19fd6028029
Keenan Smith <zoomy500@gmail.com>
2016-12-23 15:40:10 +0000

    Update L_Shaders.cs

--- zoomy/L_Shaders.cs
+++ zoomy/L_Shaders.cs
@@ -6,6 +6,14 @@ namespace zoomy
 {
     public class L_Shaders
     {
-        
+        void Start()
+        {
+            Settings.shaders = Settings.bundle.LoadAllAssets<Shader>();
+         
+            foreach(Shader shader in Settings.shaders)
+            {
+                console.log("Shader: " + shader.name);
+            }
+        }
     }
 }
